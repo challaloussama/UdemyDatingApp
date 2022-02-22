2b6db4d59e679db4d843933294e48d7705e0d5c7
oussama challal <oussama.challal@inetum.com>
2022-02-22 01:37:08 +0100

    end of section 2

---
+++ UdemyLearning/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UdemyLearning.Entities;
+
+namespace UdemyLearning.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions options) :base(options)
+        {
+
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+
+ 
+} 
