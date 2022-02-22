7864b253b75f2e07dcfa101e026d85298dc24eb5
oussama challal <oussama.challal@inetum.com>
2022-02-22 17:22:07 +0100

    angular implementation

--- UdemyLearning/Controllers/WeatherForecastController.cs
+++ UdemyLearning/Controllers/WeatherForecastController.cs
@@ -5,11 +5,11 @@ namespace UdemyLearning.Controllers
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
-    {
+    { 
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
